0b703e01ad835786bb8ccefe78b73219df72b040
Ajay Guru <ajaygur.iiitv@gmail.com>
2020-12-29 19:00:18 +0530

    feature4: adder should be able to indentify any non negative numbers and throw exception. Unit Test1: Passing Unit Test2: Passing Unit Test3: Passing Unit Test4: Passing Unit Test5: Passing Unit Test6: Passing

--- test/unit_test5.py
+++ test/unit_test5.py
@@ -7,5 +7,5 @@ adder should be able to indentify any non negative numbers and throw exception.
 '''
 
 add = string_adder("1","/n-2")
-print("Expected Result: ", 1+2+3+4)
+print("Expected Result: ", "Exception")
 print("Result from string adder: ", add)

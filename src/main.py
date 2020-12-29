fe370e7f753ec14776b2e4a7594e5621153540ae
Ajay Guru <ajaygur.iiitv@gmail.com>
2020-12-29 17:09:01 +0530

    first unit test passing

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd 
+
+
+def string_adder(string1,string2):
+    """
+    docstring
+    """
+    check_flag_1 = 1
+    check_flag_2 = 1
+    result = 0
+    if not string1:
+        check_flag_1 = 0
+    if not string2:
+        check_flag_2 = 0
+    if(check_flag_1 == 1 and check_flag_2 == 1):
+        result = int(string1) + int(string2)
+    elif(check_flag_1 == 0 and check_flag_2 == 1):
+        elements_2 = string2.split(",")
+        for element in elements_2:
+            result = result + int(element)
+    elif(check_flag_1 == 1 and check_flag_2 == 0):
+        elements_1 = string1.split(",")
+        for element in elements_1:
+            result = result + int(element)
+
+    return result
+
+
+
+if __name__ == "__main__":
+    adder = string_adder("1","2")
